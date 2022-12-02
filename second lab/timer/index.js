0ab6d1894dadc2c1379612e826c563b98a59a2b7
Vechernin Rostislav <even644007@gmail.com>
2022-12-02 17:29:35 +0600

    fix: user can change time after reloading
    + refactoring of index.js

--- second lab/timer/index.js
+++ second lab/timer/index.js
@@ -15,123 +15,134 @@ let seconds = 0;
 let minutes = 0;
 
 if (localStorage.seconds) {
-  seconds = parseInt(localStorage.seconds);
+    seconds = parseInt(localStorage.seconds);
 } else {
-  localStorage.setItem("seconds", 0);
+    localStorage.setItem("seconds", 0);
 }
 if (localStorage.minutes) {
-  minutes = parseInt(localStorage.minutes);
+    minutes = parseInt(localStorage.minutes);
 } else {
-  localStorage.setItem("minutes", 0);
+    localStorage.setItem("minutes", 0);
 }
 if (!localStorage.isStarted) {
-  localStorage.setItem("isStarted", false);
+    localStorage.setItem("isStarted", false);
 }
 
 const inputsOnChange = (min, sec) => {
-  const min1 = parseInt(min);
-  const sec1 = parseInt(sec);
+    const min1 = parseInt(min);
+    const sec1 = parseInt(sec);
 
-  if (min1<10) {
-    minInput.value = "0" + min1;
-  } else {
-    minInput.value = min1;
-  }
+    if (min1 < 10) {
+        minInput.value = "0" + min1;
+    } else {
+        minInput.value = min1;
+    }
 
-  if (sec1<10) {
-    secInput.value = "0" + sec1;
-  } else {
-    secInput.value = sec1;
-  }
+    if (sec1 < 10) {
+        secInput.value = "0" + sec1;
+    } else {
+        secInput.value = sec1;
+    }
 
-  localStorage.seconds = sec1;
-  localStorage.minutes = min1;
+    localStorage.seconds = sec1;
+    localStorage.minutes = min1;
 }
 
 const onFinish = () => {
-  if (localStorage.isStarted === "true") {
-    audio.play()
-    isStopped = true;
-    stopButton.disabled = true;
-    resetButton.disabled = false;
-    
-    minInput.readOnly = false;
-    secInput.readOnly = false;
+    if (localStorage.isStarted === "true") {
+        audio.play()
+        isStopped = true;
+        stopButton.disabled = true;
+        resetButton.disabled = false;
 
-    body.classList.add("onFinish");
-  }  
+        minInput.readOnly = false;
+        secInput.readOnly = false;
+
+        body.classList.add("onFinish");
+    }
 }
 
 const timer = setInterval(() => {
-  if (!isStopped && timeLeft >= 0) {
-  inputsOnChange(timeLeft/60, timeLeft%60)
-  timeLeft -= 1;
-  } else if (parseInt(localStorage.minutes) === 0 && parseInt(localStorage.seconds) === 0) {
-    onFinish();
-  }
+    if (!isStopped && timeLeft >= 0) {
+        inputsOnChange(timeLeft / 60, timeLeft % 60)
+        timeLeft -= 1;
+    } else if (parseInt(localStorage.minutes) === 0 && parseInt(localStorage.seconds) === 0) {
+        onFinish();
+    }
 }, 1000)
 
 const startTimer = () => {
-  timeLeft = parseInt(secInput.value) + parseInt(minInput.value)*60;
+    timeLeft = parseInt(secInput.value) + parseInt(minInput.value) * 60;
 
-  if (timeLeft != 0){
-    isStopped = false;
+    if (timeLeft != 0) {
+        isStopped = false;
 
-    stopButton.disabled = false;
-    resetButton.disabled = true;
+        stopButton.disabled = false;
+        resetButton.disabled = true;
 
-    oneMin.disabled = true;
-    fiveMin.disabled = true;
-    tenMin.disabled = true;
-    
-    minInput.readOnly = true;
-    secInput.readOnly = true;
+        oneMin.disabled = true;
+        fiveMin.disabled = true;
+        tenMin.disabled = true;
 
-    localStorage.isStarted = "true";
-  }
+        minInput.readOnly = true;
+        secInput.readOnly = true;
+
+        localStorage.isStarted = "true";
+    }
 
 }
 
 const stopTimer = () => {
-  isStopped = true;
-  resetButton.disabled = false;
-  stopButton.disabled = true;
+    localStorage.isStarted = "false";
+
+    isStopped = true;
+    resetButton.disabled = false;
+    stopButton.disabled = true;
 }
 
 const resetTimer = () => {
-  localStorage.isStarted = "false";
+    localStorage.isStarted = "false";
 
-  isStopped = true;
-  oneMin.disabled = false;
-  fiveMin.disabled = false;
-  tenMin.disabled = false;
-  stopButton.disabled = true;
-  minInput.readOnly = false;
-  secInput.readOnly = false;
+    isStopped = true;
 
-  localStorage.minutes = 0;
-  localStorage.seconds = 0;
-  inputsOnChange(localStorage.minutes, localStorage.seconds);
-  body.classList.remove("onFinish");
+    oneMin.disabled = false;
+    fiveMin.disabled = false;
+    tenMin.disabled = false;
+
+    stopButton.disabled = true;
+
+    minInput.readOnly = false;
+    secInput.readOnly = false;
+
+    localStorage.minutes = 0;
+    localStorage.seconds = 0;
+
+    inputsOnChange(localStorage.minutes, localStorage.seconds);
+    body.classList.remove("onFinish");
 }
 
 const addMinutes = (mins) => {
-  inputsOnChange(parseInt(localStorage.minutes) + mins, localStorage.seconds);
+    inputsOnChange(parseInt(localStorage.minutes) + mins, localStorage.seconds);
 }
 
 const inputChange = () => {
-  inputsOnChange(minInput.value, secInput.value);
+    inputsOnChange(minInput.value, secInput.value);
 }
 
 stopButton.disabled = true;
+resetButton.disabled = false;
+
 oneMin.disabled = true;
 fiveMin.disabled = true;
 tenMin.disabled = true;
-resetButton.disabled = false;
+
+minInput.readOnly = true;
+secInput.readOnly = true;
 
 inputsOnChange(minutes, seconds);
+
 if (localStorage.isStarted === "true") {
-  startTimer();
+    startTimer();
 }
 
 startButton.addEventListener("click", startTimer);

88b6343b3ad077b44ec7a4fa8460ded082e96d3e
Vechernin Rostislav <even644007@gmail.com>
2022-12-16 19:31:19 +0600

    feat: init commit

---
+++ react-lab/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { Normalize } from 'styled-normalize';
+
+const App: FC = () => {
+  return (
+    <>
+      <Normalize />
+      <div>Hello World!</div>
+    </>
+  );
+};
+
+export default App;
